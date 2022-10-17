4255b174597152b9dbb629682547d3c4971bc9ad
Dog-Egg <Dog-Egg@users.noreply.github.com>
2022-10-17 05:45:33 +0000

    deploy: b3488fa20e4acf3372fbbed56a24eb40b1993d21

---
+++ assets/js/1df93b7f.9b16962b.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[237],{69754:(e,n,t)=>{t.r(n),t.d(n,{default:()=>o});var a=t(67294),c=t(76775);function o(){return a.createElement(c.l_,{to:"/django-openapi/intro"})}}}]);
